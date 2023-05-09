517bfc1cb9b67317d363b8e1bdf0ac7fb78a3755
Mostafizur <mostafizurdh2000@gmail.com>
2023-05-09 17:34:01 +0600

    menu responsive

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 const menuIcon = document.querySelector(".menu_icon")
+const mobileMenu = document.querySelector(".mobile_menu")
 menuIcon.addEventListener("click", ()=>{
-    
+    mobileMenu.classList.toggle("active")
 })
